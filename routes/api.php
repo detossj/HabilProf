c5e9eba4e93e571b3490492b53143e5374b7bb06
detossj <jorgerubilarssj@gmail.com>
2025-10-20 21:39:16 -0300

    added routes to api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+
+Route::prefix('v1')->group(function () {
+
+    //::auth
+    Route::post('/auth/register',[AuthController::class, 'register']);
+    Route::post('/auth/login',[AuthController::class, 'login']);
+
+    //Private
+    Route::group(['middleware' => 'auth:sanctum'], function () {
+        //::auth
+        Route::post('/auth/logout',[AuthController::class, 'logout']);
+    });
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
