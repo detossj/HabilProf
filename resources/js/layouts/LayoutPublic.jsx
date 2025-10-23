d076125446a14242619e2adc03a7900f33cf3f35
detossj <jorgerubilarssj@gmail.com>
2025-10-22 21:44:00 -0300

    changed LayoutPublic.jsx

--- resources/js/layouts/LayoutPublic.jsx
+++ resources/js/layouts/LayoutPublic.jsx
@@ -5,11 +5,13 @@ import Footer from '../components/Footer'
 
 const LayoutPublic = () => {
   return (
-    <>
-        <NavBar/>
-        <Outlet/>
+    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+        {/* Esto empuja el Footer al final y hace que se pueda centrar el login */}
+        <div style={{ flexGrow: 1, display: 'flex' }}>
+            <Outlet/>
+        </div>
         <Footer/>
-    </>
+    </div>
   )
 }
 
