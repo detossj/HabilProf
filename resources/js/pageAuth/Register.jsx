2e52d42573b7d3fadfd7c9204171fbd543c464db
Jorge Rubilar <jorgerubilarssj@gmail.com>
2025-10-23 10:55:28 -0300

    created Register.jsx

---
+++ resources/js/pageAuth/Register.jsx
@@ -0,0 +1,134 @@
+
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import Config from '../Config'
+import AuthUser from './AuthUser'
+import axios from 'axios'
+
+import './LoginAndRegister.css'; 
+import ucscLogo from '../../assets/images/Logo-UCSC-Color-Horizontal.png'; 
+
+const Register = () => {
+
+    const { getToken, setToken } = AuthUser()
+
+    const [rut, setRut] = useState("")
+    const [password, setPassword] = useState("")
+    const [passwordConfirmation, setpasswordConfirmation] = useState("")
+    const [message, setMessage] = useState("")
+  
+    const navigate = useNavigate()
+  
+    useEffect(() => {
+      if(getToken()) {
+        navigate("/")
+      }
+    },[])
+  
+    const submitLogin = async(e) => {
+      e.preventDefault();
+      await axios.get('/sanctum/csrf-cookie').then((response) => {
+        Config.getLogin({rut,password})
+        .then(({data}) => {
+          if(data.success) {
+            setToken(data.user, data.token, data.user.roles[0].name)
+          }
+          else {
+            setMessage(data.data.message)
+          }
+        })
+      })
+    }
+
+    return (
+        <div className='container d-flex justify-content-center align-items-center' >
+            <div className='col-sm-10 col-md-6 col-lg-4'>
+                <div className='card shadow-lg login-card' >
+                    <div className='card-body d-flex flex-column align-items-center'>
+                        
+                        {/* Logo y Título */}
+                        <div className='text-center mb-4'>
+                            <img 
+                                src={ucscLogo} 
+                                alt="UCSC Logo" 
+                                style={{ width: '150px', height: 'auto' }} 
+                                className='d-block mx-auto mb-3' 
+                            />
+                          
+                            <h2 className='fw-bold text-title-portal'>
+                              HabilProf
+                            </h2> 
+                            <h2 className='fw-bold text-subtitle-portal'>
+                              Registro
+                            </h2> 
+                        </div>
+                        
+                        {/* Campo RUT */}
+                        <div className="w-100 mb-3"> 
+                            <label htmlFor="rut-input" className='form-label w-100 text-start login-label'>
+                              RUT
+                            </label>
+                            <input 
+                                id="rut-input"
+                                type="text" 
+                                className='form-control login-input-control' 
+                                placeholder='Sin puntos, ni dígito verificador'
+                                value={rut} 
+                                onChange={(e)=>setRut(e.target.value)}
+                                required
+                            />
+                        </div>
+                        
+                        {/* Campo Contraseña */}
+                        <div className="w-100 mb-4">
+                            <label htmlFor="password-input" className='form-label w-100 text-start login-label'>
+                              Contraseña
+                            </label>
+                            <input 
+                                id="password-input"
+                                type="password" 
+                                className='form-control login-input-control' 
+                                placeholder='Contraseña' 
+                                value={password}
+                                onChange={(e)=>setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+
+                        {/* Campo confirmacion Contraseña */}
+                        <div className="w-100 mb-4">
+                            <label htmlFor="passwordConfirmation-input" className='form-label w-100 text-start login-label'>
+                              Confirmación Contraseña
+                            </label>
+                            <input 
+                                id="passwordConfirmation-input"
+                                type="password" 
+                                className='form-control login-input-control' 
+                                placeholder='Confirmación Contraseña' 
+                                value={passwordConfirmation}
+                                onChange={(e)=>setpasswordConfirmation(e.target.value)}
+                                required
+                            />
+                        </div>
+                        
+                        {/* Botón INGRESAR */}
+                        <button className='btn w-100 btn-ucsc-red' onClick={submitLogin} >
+                          CREAR CUENTA
+                        </button>
+
+                        <div className='w-100 text-center mt-3'>
+                            <p className='d-inline'>Si ya tiene cuenta, </p>
+                            <a href="/" className='d-inline fw-bold text-primary text-decoration-none'>
+                                Inicie sesión aquí
+                            </a>
+                        </div>
+
+                        <p className='text-center mt-3'>{message}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+      )
+}
+
+export default Register
