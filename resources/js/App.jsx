919d8e6b4530d7f57a95a2a1957899eb777b7254
detossj <jorgerubilarssj@gmail.com>
2025-10-22 21:41:24 -0300

    changed route in App.jsx

--- resources/js/App.jsx
+++ resources/js/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
 
         <Route path="/" element={<LayoutPublic/>}>
 
-          <Route path='/login' element={ <Login/> } />
+          <Route index element={ <Login/> } />
         
         </Route>
       
