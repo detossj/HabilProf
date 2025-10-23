1ec3e8808db3d23f0400f298daeea07efd9d17e9
detossj <jorgerubilarssj@gmail.com>
2025-10-22 21:44:44 -0300

    changed Login.jsx

--- resources/js/pageAuth/Login.jsx
+++ resources/js/pageAuth/Login.jsx
@@ -1,27 +1,64 @@
-import React from 'react'
+import React from 'react';
+import './Login.css'; 
+
+import ucscLogo from '../../assets/images/Logo-UCSC-Color-Horizontal.png'; 
 
 const Login = () => {
     return (
-        <div className='container'>
-          <div className='row justify-content-center'>
-            <div className='col-sm-4'>
-              <div className='card mt-5 mb-5'>
-                <div className='card-body'>
-    
-                  <h1 className='text-center fw-bolder'>Login</h1>
-    
-                  <input type="text" className='form-control mt-3' placeholder='Email:' required/>
-    
-                  <input type="password" className='form-control mt-3' placeholder='Password:' required/>
-    
-                  <button  className='btn btn-primary w-100 mt-3'>SEND</button>
-    
-                  <p className='text-center mt-3'></p>
-
+        <div className='container d-flex justify-content-center align-items-center' >
+            <div className='col-sm-10 col-md-6 col-lg-4'>
+                <div className='card shadow-lg login-card' >
+                    <div className='card-body d-flex flex-column align-items-center'>
+                        
+                        {/* Logo y Título */}
+                        <div className='text-center mb-4'>
+                            <img 
+                                src={ucscLogo} 
+                                alt="UCSC Logo" 
+                                style={{ width: '150px', height: 'auto' }} 
+                                className='d-block mx-auto mb-3' 
+                            />
+                          
+                            <h2 className='fw-bold text-portal'>
+                              HabilProf
+                            </h2> 
+                        </div>
+                        
+                        {/* Campo RUT */}
+                        <div className="w-100 mb-3"> 
+                            <label htmlFor="rut-input" className='form-label w-100 text-start login-label'>
+                              RUT
+                            </label>
+                            <input 
+                                id="rut-input"
+                                type="text" 
+                                className='form-control login-input-control' 
+                                placeholder='Sin puntos, ni dígito verificador' 
+                                required
+                            />
+                        </div>
+                        
+                        {/* Campo Contraseña */}
+                        <div className="w-100 mb-4">
+                            <label htmlFor="password-input" className='form-label w-100 text-start login-label'>
+                              Contraseña
+                            </label>
+                            <input 
+                                id="password-input"
+                                type="password" 
+                                className='form-control login-input-control' 
+                                placeholder='Contraseña' 
+                                required
+                            />
+                        </div>
+                        
+                        {/* Botón INGRESAR */}
+                        <button className='btn w-100 btn-ucsc-red' >
+                          INGRESAR
+                        </button>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
       )
 }
