9baf4694655336e9be196dd6e1d3c338ecf22f8b
facuna-m <facuna@ing.ucsc.cl>
2025-10-26 18:17:55 -0300

    agregé el TopBar y estilos a LayoutAdministrador

--- resources/js/layouts/LayoutAdministrador.jsx
+++ resources/js/layouts/LayoutAdministrador.jsx
@@ -2,28 +2,34 @@ import React, { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from '../components/Footer'
 import AuthUser from '../pageAuth/AuthUser'
+import TopBar from '../components/TopBar'
 
 const LayoutAdministrador = () => {
 
-  const { getRol } = AuthUser()
-  const navigate = useNavigate()
+    const { getRol } = AuthUser()
+    const navigate = useNavigate()
 
-  useEffect(() => {
-    if(getRol() != "administrador") {
-      navigate("/")
-    }
-  },[])
+    useEffect(() => {
+      if(getRol() != "administrador") {
+        navigate("/")
+      }
+    },[])
 
-  return (
-    <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-        {/* Esto empuja el Footer al final y hace que se pueda centrar  */}
-        <div style={{ flexGrow: 1, display: 'flex' }}>
-          <h1>LayoutAdministrador</h1>
-            <Outlet/>
-        </div>
-        <Footer/>
-    </div>
-  )
+    return (
+      <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+          <TopBar/>
+          <div style={{ 
+              flexGrow: 1, 
+              display: 'flex', 
+              flexDirection: 'column',
+              justifyContent: 'center', // Centra verticalmente el contenido del Outlet
+              alignItems: 'center'      // Centra horizontalmente el contenido del Outlet (el formulario)
+          }}>
+              <Outlet/>
+          </div>
+          <Footer/>
+      </div>
+    )
 }
 
 export default LayoutAdministrador
