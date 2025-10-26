619ab660765b8d6eacd51a38faef6bbd9204a29e
facuna-m <facuna@ing.ucsc.cl>
2025-10-26 18:18:51 -0300

    cambié estilos a LayoutPublic

--- resources/js/layouts/LayoutPublic.jsx
+++ resources/js/layouts/LayoutPublic.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import NavBar from '../components/NavBar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../components/Footer'
 
@@ -7,7 +6,13 @@ const LayoutPublic = () => {
   return (
     <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
         {/* Esto empuja el Footer al final y hace que se pueda centrar el login */}
-        <div style={{ flexGrow: 1, display: 'flex' }}>
+        <div style={{ 
+              flexGrow: 1, 
+              display: 'flex', 
+              flexDirection: 'column',
+              justifyContent: 'center', // Centra verticalmente el contenido del Outlet
+              alignItems: 'center'      // Centra horizontalmente el contenido del Outlet (el formulario)
+          }}>
             <Outlet/>
         </div>
         <Footer/>
