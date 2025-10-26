718c1847f665da334845c0bb3e6c988dff970286
facuna-m <facuna@ing.ucsc.cl>
2025-10-26 18:18:07 -0300

    agregé el TopBar y estilos a LayoutAlumno

--- resources/js/layouts/LayoutAlumno.jsx
+++ resources/js/layouts/LayoutAlumno.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import Footer from '../components/Footer'
 import { Outlet, useNavigate } from 'react-router-dom'
 import AuthUser from '../pageAuth/AuthUser'
+import TopBar from '../components/TopBar'
 
 const LayoutAlumno = () => {
 
@@ -17,8 +18,14 @@ const LayoutAlumno = () => {
   return (
     <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
         {/* Esto empuja el Footer al final y hace que se pueda centrar  */}
-        <div style={{ flexGrow: 1, display: 'flex' }}>
-        <h1>LayoutAlumno</h1>
+        <div style={{ 
+              flexGrow: 1, 
+              display: 'flex', 
+              flexDirection: 'column',
+              justifyContent: 'center', // Centra verticalmente el contenido del Outlet
+              alignItems: 'center'      // Centra horizontalmente el contenido del Outlet (el formulario)
+          }}>
+          <TopBar/>
             <Outlet/>
         </div>
         <Footer/>
