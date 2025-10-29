47029f09aef276a05538b80be5cb000535ed2601
detossj <jorgerubilarssj@gmail.com>
2025-10-29 19:45:03 -0300

    added SiderBar and changed styles in LayoutAdministrador.jsx

--- resources/js/layouts/LayoutAdministrador.jsx
+++ resources/js/layouts/LayoutAdministrador.jsx
@@ -3,32 +3,27 @@ import { Outlet, useNavigate } from 'react-router-dom'
 import Footer from '../components/Footer'
 import AuthUser from '../pageAuth/AuthUser'
 import TopBar from '../components/TopBar'
+import SideBar from '../pageadmin/SideBar'
 
 const LayoutAdministrador = () => {
-
     const { getRol } = AuthUser()
     const navigate = useNavigate()
 
     useEffect(() => {
-      if(getRol() != "administrador") {
-        navigate("/")
-      }
-    },[])
+        if(getRol() !== "administrador") {
+            navigate("/")
+        }
+    }, [])
 
     return (
-      <div style={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
-          <TopBar/>
-          <div style={{ 
-              flexGrow: 1, 
-              display: 'flex', 
-              flexDirection: 'column',
-              justifyContent: 'center', // Centra verticalmente el contenido del Outlet
-              alignItems: 'center'      // Centra horizontalmente el contenido del Outlet (el formulario)
-          }}>
-              <Outlet/>
-          </div>
-          <Footer/>
-      </div>
+        <div className="d-flex flex-column min-vh-100">
+            <TopBar />
+            <SideBar />
+            <main className="flex-grow-1 d-flex flex-column justify-content-start align-items-center p-3">
+                <Outlet />
+            </main>
+            <Footer />
+        </div>
     )
 }
 
