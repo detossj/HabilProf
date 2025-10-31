2995c4aa98d6de44baba025f9527ec48ff34626b
detossj <jorgerubilarssj@gmail.com>
2025-10-30 22:16:55 -0300

    changed styles in Footer.jsx

--- resources/js/components/Footer.jsx
+++ resources/js/components/Footer.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='container d-flex justify-content-center align-items-center mb-5'>
+    <div className='container d-flex justify-content-center align-items-center mb-5' 
+    style={{
+      color: 'white',
+      fontWeight: 'bold',
+      textShadow: '2px 2px 4px rgba(0,0,0,0.7)',
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: '1rem',
+    }}
+    >
       © 2025 UCSC. Todos los derechos reservados
     </div>
   )
