b9511ab2bb16152db59ecd28cdbe88a3e6246897
facuna-m <facuna@ing.ucsc.cl>
2025-10-30 23:42:09 -0300

    modifiqué TopBar.jsx

--- resources/js/components/TopBar.jsx
+++ resources/js/components/TopBar.jsx
@@ -3,7 +3,6 @@ import ucscLogo from '../../assets/images/Logo-UCSC-Color-Horizontal.png';
 import AuthUser from '../pageAuth/AuthUser';
 
 const TopBar = () => {
-    // Nombre de usuario fijo para el ejemplo
     const {getUser, getLogout} = AuthUser()
     const user = getUser();
     const userName = user ? user.name : '';
@@ -12,7 +11,6 @@ const TopBar = () => {
             <div className="container-fluid"> 
                 {/* Sección del Logo (Izquierda) */}
                 <a className="navbar-brand d-flex align-items-center" href="/">
-                    {/* Texto UCSC como placeholder del logo */}
                     <img 
                         src={ucscLogo} 
                         alt="UCSC Logo" 
@@ -21,17 +19,34 @@ const TopBar = () => {
                     />
                 </a>
 
+                <div className="app-title-container">
+                    <h4 className="mb-0 fw-bold app-title-text" style={{
+                        fontSize: '1.1rem',
+                        fontWeight: '100',
+                        color: '#ffffffff',
+                        letterSpacing: '1px',
+                        textTransform: 'uppercase',
+                        marginBottom: '2px',
+                        backgroundColor: '#dc3545',
+                        padding: '4px 8px',
+                        borderRadius: '7px',
+                        display: 'inline-block'
+                    }}>
+                        HabilProf
+                    </h4>
+                    <small className="app-subtitle-text" style={{
+                        fontSize: '0.8rem',
+                        color: '#6c757d',
+                        letterSpacing: '0.5px',
+                        fontWeight: '400',
+                        display: 'block'
+                    }}>
+                        Sistema de Gestión de Habilitaciones Profesionales
+                    </small>
+                </div>
+
                 {/* Sección del Usuario (Derecha) */}
                 <ul className="navbar-nav ms-auto d-flex align-items-center">
-                    {/* Ícono de Horario */}
-                    <li className="nav-item">
-                        <a className="nav-link" href="#" title="Horario">
-                            {/* Placeholder para el ícono de Horario */}
-                            <i className="bi bi-calendar-check" style={{ fontSize: '1.2rem' }}></i> 
-                        </a>
-                    </li>
-                    
-                    {/* Nombre del Usuario */}
                     <li className="nav-item">
                         <span className="navbar-text me-3">
                             {userName}
